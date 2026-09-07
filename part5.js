a25fa5fd7dee335710e63b4292f04e2f95777a63
myers-source <muhammadumairrr213@gmail.com>
2026-09-07 13:35:20 +0700

    Add grading logic based on nilai value

---
+++ part5.js
@@ -0,0 +1,14 @@
+let nilai = 95;
+
+if (nilai >= 90) {
+    console.log("kamu dapat A");
+}
+else if (nilai >= 80) {
+    console.log("kamu dapat B");
+}
+else if (nilai >= 70) {
+    console.log("kamu dapat C");
+}
+else {
+    console.log("kamu dapat D");
+}
