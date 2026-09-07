7d3b127f747674abdfe8cb25be48c9fc9377b6b8
myers-source <muhammadumairrr213@gmail.com>
2026-09-07 13:33:06 +0700

    Add main function to evaluate student grades

---
+++ part4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int nilai = 80;
+    if (nilai == 100)
+    {
+        cout << "nilai sempurna";
+    }
+    else if (nilai >= 75) 
+    {
+        cout << "Selamat kamu lulus";
+    }
+    else;
+    {
+        cout << "maaf silahkan coba lagi minggu depan";
+    }
+
+    return 0;
+}
