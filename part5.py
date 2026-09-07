8a963391d0db43965fa51a163de932568188d226
myers-source <muhammadumairrr213@gmail.com>
2026-09-07 13:35:46 +0700

    Add grading system based on nilai

---
+++ part5.py
@@ -0,0 +1,10 @@
+nilai = 95
+
+if nilai >= 90:
+    print("kamu dapat A")
+elif nilai >= 80:
+    print("kamu dapat B")
+elif nilai >= 70:
+    print("kamu dapat C")
+else:
+    print("kamu dapat D")
