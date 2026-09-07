657e6cd5658a93cbb8f20864570a3232dc860a5b
myers-source <muhammadumairrr213@gmail.com>
2026-09-07 13:25:41 +0700

    Add pricing calculation with discount in part1.py

---
+++ part1.py
@@ -0,0 +1,12 @@
+harga_buku = 15000
+pulpen = 3750
+tas = 120000
+total_setelah_diskon = ((harga_buku * 3) + (pulpen * 2) + tas) * 0.9
+
+  print("total harga:")
+  print(str((harga_buku * 3) + (pulpen * 2) + tas) + " Rupiah")
+
+  print("---------------------")
+
+  print("Harga setelah diskon 10%")
+  print(total_setelah_diskon)
