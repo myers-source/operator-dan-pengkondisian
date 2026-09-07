fadd2f282f2c2aef439c17d50417df8b6bbca61f
myers-source <muhammadumairrr213@gmail.com>
2026-09-07 13:26:22 +0700

    Add pricing calculation with discount in part1.js

---
+++ part1.js
@@ -0,0 +1,16 @@
+let harga_buku = 15000;
+  let pulpen = 3750;
+let tas = 120000;
+  let total_setelah_diskon = ((harga_buku * 3) + (pulpen * 2) + tas) * 0.9;
+
+{
+    console.log("total harga:");
+    console.log(((harga_buku * 3) + (pulpen * 2) + tas) + " Rupiah");
+}
+{
+    console.log("---------------------");
+}
+{
+    console.log("Harga setelah diskon 10%");
+    console.log(total_setelah_diskon);
+}
