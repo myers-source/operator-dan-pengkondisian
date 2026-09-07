5aac69c9900bd282fc84e8978b405422a1074e4a
myers-source <muhammadumairrr213@gmail.com>
2026-09-07 13:31:24 +0700

    Add age verification program in part3.cpp

---
+++ part3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int umur = 18;
+    if(umur >= 17)
+    {
+    cout << "Anda sudah bisa buat KTP";
+    }
+    else
+    {
+    cout << "Anda belum cukup umur untuk membuat ktp";
+    }
+    return 0;
+}
